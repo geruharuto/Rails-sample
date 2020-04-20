d725b4e35aec3afa76706ba2352f2bd5e77d406b
geruharuto <geruharuto2121@yahoo.co.jp>
2020-04-20 20:58:03 +0900

    [Finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,10 +5,16 @@ class TodolistsController < ApplicationController
   def create
   	list = List.new(list_params)
   	list.save
-  	redirect_to "/top"
+  	redirect_to todolist_path(list.id)
   end
-  private
-
+  def index
+  	@lists = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+  end
+ 
+ private
   def list_params
   	params.require(:list).permit(:title, :body)
   end
