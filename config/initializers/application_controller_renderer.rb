f065dba2fb1fb18fe1f73a91a31ea88201607588
geruharuto <geruharuto2121@yahoo.co.jp>
2020-04-21 17:22:31 +0900

    [Finihs]3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7552091df9e6cb7ba7363bceb6c018928679c277a5e7fb5def2ae786d134fead1ec93d8ebaa953dd8e321112b763a4a8034fb8940b587e55554d7adb3d8b443b'
